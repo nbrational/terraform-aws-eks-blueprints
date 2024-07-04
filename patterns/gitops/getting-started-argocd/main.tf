b2429688fa1b64c67f386e524331ea9a491e9b78
Nitin Bhalla <nitinbhalla@Nitins-Laptop.local>
2024-07-05 09:30:34 +1000

    add filter for AZ to not use local zones

--- patterns/gitops/getting-started-argocd/main.tf
+++ patterns/gitops/getting-started-argocd/main.tf
@@ -2,7 +2,13 @@ provider "aws" {
   region = local.region
 }
 data "aws_caller_identity" "current" {}
-data "aws_availability_zones" "available" {}
+
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
 
 provider "helm" {
   kubernetes {
